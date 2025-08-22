4cfe7bc908251f5bb1e7fc8b5e1646aee9a0f174
Badwolfpup <adam_folke@yahoo.se>
2025-08-22 09:11:19 +0200

    Add project files.

---
+++ Chess/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chess
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public ObservableCollection<Square> Squares { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeBoard();
+            
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void InitializeBoard()
+        {
+            Squares = new();
+            for (int row = 0; row < 8; row++)
+            {
+                for (int col = 0; col < 8; col++)
+                {
+                    var squareColor = (row + col) % 2 == 0 ? Brushes.White : Brushes.Black;
+                    var square = new Square(squareColor, new Point(col, row));
+                    Squares.Add(square);
+                    // Here you would typically add the square to a grid or canvas in your UI
+                    // For example: chessBoard.Children.Add(square);
+                }
+            }
+        }
+    }
+}
---
+++ Chess/Square.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+
+
+namespace Chess
+{
+    public class Square
+    {
+        public SolidColorBrush BlackOrWhite { get; }
+
+        public IPiece Piece { get; set; }
+
+        public Point Position { get; set; }
+
+        public Square(SolidColorBrush blackOrWhite, Point position)
+        {
+            BlackOrWhite = blackOrWhite;
+            Position = position;
+        }
+    }
+}
